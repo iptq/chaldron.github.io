b9dc5bf0c3cdcf734dd33a9d31e328f81d9e9d90
Aditya <moonsketch@me.com>
2015-05-19 10:57:54 -0500

    Added a script to simplify tedious work

---
+++ dank.py
@@ -0,0 +1,9 @@
+array = ['Allied Soccer', 'Baseball', 'Basketball', 'Bowling', 'Cross Country', 'Fencing', 'Football', 'Gold', 'Gymnastics', 'Ice Hockey', 'Lacrosse', 'Soccer', 'Swimming and Diving', 'Tennis', 'Track and Field', 'Volleyball', 'Wrestling', 'Water Polo']
+
+for sport in array:
+    print("\n\n\n\n")
+    print(sport + "\n\n")
+    print('<div class="tab-container"><h5 class="tab-heading">' + sport + '</h5> <p>Season: ' + input("Season: ") + '</p> <p>Start Date: ' + input("Start Date: ") + '</p> <p>Levels: ' + input("Levels: ") + '</p> <p>Coach(es): ' + input("Coaches: ")+ '</p> <p>Informational Meeting:' + input("Informational Meeting: ") + '</p> <p>Commitment: ' + input("Commitment: ") + '</p> <p>Athletic Waiver required.</p></div>')
+    print("\n\n\n\n\n\n")
+
+
