d7996f66f6f95d151c64b1a5e3dceff3c646a203
Aditya <moonsketch@me.com>
2015-05-20 10:43:10 -0500

    Updated script

--- dank.py
+++ dank.py
@@ -3,7 +3,7 @@ array = ['Allied Soccer', 'Baseball', 'Basketball', 'Bowling', 'Cross Country', 
 for sport in array:
     print("\n\n\n\n")
     print(sport + "\n\n")
-    print('<div class="tab-container"><h5 class="tab-heading">' + sport + '</h5> <p>Season: ' + input("Season: ") + '</p> <p>Start Date: ' + input("Start Date: ") + '</p> <p>Levels: ' + input("Levels: ") + '</p> <p>Coach(es): ' + input("Coaches: ")+ '</p> <p>Informational Meeting:' + input("Informational Meeting: ") + '</p> <p>Commitment: ' + input("Commitment: ") + '</p> <p>Athletic Waiver required.</p></div>')
+    print('<div class="tab-container"><h5 class="tab-heading">' + sport + '</h5> <p>Season: ' + input("Season: ") + '</p> <p>Start Date: ' + input("Start Date: ") + '</p> <p>Levels: ' + input("Levels: ") + '</p> <p>Coach(es): ' + input("Coaches: ")+ '</p> <p>Informational Meeting: ' + input("Informational Meeting: ") + '</p> <p>Commitment: ' + input("Commitment: ") + '</p> <p>Athletic Waiver required.</p></div>')
     print("\n\n\n\n\n\n")
 
 
